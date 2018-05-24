fbacebbe8bede6ae7d1b86932829e0b10078174d
ShellOliver <shelldroid01@gmail.com>
2018-05-24 13:58:18 -0300

    with crud

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# nestjs-generator v2.0.2
+# nestjs-generator v2.0.0
 
 The [nestjs-generator](https://nestjs.com) generated modules for you NestJS Application
-Based on https://github.com/bipin000/nestjs-generator
+Based on https://github.com/bipin000/nestjs-generator and https://github.com/zMotivat0r/nest-crud/
 
 ## Installation
 
 Using nestjs-generator:
 ```shell
-$ npm i -g nestjs-generator
+$ npm install
 ```
 
-Usage:
+## Usage:
 ```shell
-$ nestjs-generator module myModuleName
+$ node bin/nestjs-generator module myModuleName <options>
 
 ...generated controller...
 ...generated entity...
@@ -20,3 +20,41 @@ $ nestjs-generator module myModuleName
 ...generated module...
 
 ```
+
+## Options:
+--c: create crud api with @nestjsx/crud 
+
+
+## API Methods and Endpoints
+
+Assume you've created some CRUD controller with the route `@Controller('cats')`. In that case, Nest will create endpoints as follows:
+
+#### `GET /cats`
+
+Res Data: _array of entities; an empty array_
+<br>Res Code: _200_
+
+#### `GET /cats/:id`
+
+Req Params: `:id` - _entity id_
+<br>Res Data: _entity object_
+<br>Res Code: _200; 400; 404_
+
+#### `POST /cats`
+
+Req Body: _entity object_
+<br>Res Data: _entity object_
+<br>Res Code: _201; 400_
+
+#### `PUT /cats/:id`
+
+Req Params: `:id` - _entity id_
+<br>Req Body: _entity object_
+<br>Res Data: _entity object_
+<br>Res Code: _201; 400; 404_
+
+#### `DELETE /cats/:id`
+
+Req Params: `:id` - _entity id_
+<br>Res Data: _empty_
+<br>Res Code: _200; 400; 404_
--- bin/nestjs-generator.js
+++ bin/nestjs-generator.js
@@ -2,8 +2,7 @@
 const fs = require('fs');
 const [...args] = process.argv;
 
-//fs.mkdir('test');
-let options=`
+let options = `
 Invalid Command
     supported options:
         module
@@ -13,49 +12,61 @@ Invalid Command
 
 `;
 
-if(args[2] === undefined || args[3] === undefined){
+if (args[2] === undefined || args[3] === undefined) {
 
     console.log(options)
-    process.exit(1);    
+    process.exit(1);
 }
-
-const getFileName = (text) => {
-  let rule = new RegExp(/([A-Z])/);
-  let fileName = '';
-  for(let t of text)
-  fileName += rule.test(t) ? `-${t.toLowerCase()}` : t;
-  return fileName;
-};
 
 let option = args[2];
 let moduleName = args[3];
-switch(option){
-    case "module":
-    let moduleNameFiles=getFileName(moduleName);
-    let moduleNameLower=moduleName.charAt(0).toLowerCase() + moduleName.slice(1);
-    let moduleNameUpper= moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
-    fs.mkdir(moduleNameFiles, (err) => console.log(err));
-    
-    let controllerString=`import { Controller, Post, Body } from '@nestjs/common';
-import { ${moduleNameUpper}Service } from './${moduleNameFiles}.service';
-@Controller('${moduleNameLower}')
-export class ${moduleNameUpper}Controller {
-    constructor(private readonly ${moduleNameLower}Service: ${moduleNameUpper}Service) { }
+let ifCrud = args[4];
 
-    @Post()
-    async create(@Body() ${moduleNameLower}: ${moduleNameUpper}) {
-        return await this.${moduleNameLower}Service.create(${moduleNameLower});
+const getFileName = (text) => {
+    let rule = new RegExp(/([A-Z])/);
+    let fileName = '';
+    for (let t of text)
+        fileName += rule.test(t) ? `-${t.toLowerCase()}` : t;
+    return fileName;
+};
+
+const moduleNameFiles = getFileName(moduleName);
+const moduleNameLower = moduleName.charAt(0).toLowerCase() + moduleName.slice(1);
+const moduleNameUpper = moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+
+const setCrud = (param) => {
+    if (param == '-c') {
+        return {
+            serviceExtend: `extends CrudTypeOrmService<${moduleNameUpper}>`,
+            serviceImport: `import { CrudTypeOrmService } from '@nestjsx/crud/typeorm';`,
+            controllerExtend: `extends CrudController<${moduleNameUpper}>`,
+            controllerImport: `import { CrudController } from '@nestjsx/crud';`,
+            controllerSuper: `super(${moduleNameLower}Service);`,
+            serviceSuper: `super(${moduleNameLower}Repository);`
+        }
     }
+}
 
-    @Post('/update')
-    update(@Body() ${moduleNameLower}: ${moduleNameUpper}){
-        return await this.${moduleNameLower}Service.save(${moduleNameLower});
+    switch (option) {
+        case "module":
+            const crud = setCrud(ifCrud);
+            fs.mkdir(moduleNameFiles, (err) => console.log(err));
+
+            let controllerString = `import { Controller, Post, Body } from '@nestjs/common';
+import { ${moduleNameUpper} } from './${moduleNameFiles}.entity';
+import { ${moduleNameUpper}Service } from './${moduleNameFiles}.service';
+${crud.controllerImport}
+
+@Controller('${moduleNameLower}')
+export class ${moduleNameUpper}Controller ${crud.controllerExtend}{
+    constructor(private readonly ${moduleNameLower}Service: ${moduleNameUpper}Service) { 
+        ${crud.controllerSuper}
     }
 }
 `;
-    
-    
-    let moduleString=`import { Module } from '@nestjs/common';
+
+
+            let moduleString = `import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ${moduleNameUpper}Service } from './${moduleNameFiles}.service';
 import { ${moduleNameUpper}Controller } from './${moduleNameFiles}.controller';
@@ -68,8 +79,8 @@ import { ${moduleNameUpper} } from './${moduleNameFiles}.entity';
 })
 export class ${moduleNameUpper}Module { }
 `;
-    
-    let entityString=`import {Entity, PrimaryGeneratedColumn} from 'typeorm';
+
+            let entityString = `import {Entity, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity()
 export class ${moduleNameUpper} {
@@ -77,53 +88,46 @@ export class ${moduleNameUpper} {
   id: number;
 }
 `;
-    
-    let serviceString=`import { Component } from '@nestjs/common';
+
+            let serviceString = `import { Component } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ${moduleNameUpper} } from './${moduleNameFiles}.entity';
 import { Repository } from 'typeorm/repository/Repository';
+${crud.serviceImport}
 
 @Component()
-export class ${moduleNameUpper}Service {
+export class ${moduleNameUpper}Service ${crud.serviceExtend}{
   constructor(
     @InjectRepository(${moduleNameUpper})
     private readonly ${moduleNameLower}Repository: Repository<${moduleNameUpper}>
-  ) { }
-
-  async create(${moduleNameLower}: ${moduleNameUpper}) {
-    return await this.${moduleNameLower}Repository.create(${moduleNameLower});
-  }
-
-  async update(${moduleNameLower}: ${moduleNameUpper}) {
-    return await this.${moduleNameLower}Repository.save(${moduleNameLower});
-  }
+  ) { ${crud.serviceSuper} }
 }
 `;
-    let controllerFile=`${moduleNameFiles}/${moduleNameFiles}.controller.ts`;
-    let entityFile = `${moduleNameFiles}/${moduleNameFiles}.entity.ts`;
-    let serviceFile = `${moduleNameFiles}/${moduleNameFiles}.service.ts`;
-    let moduleFile =  `${moduleNameFiles}/${moduleNameFiles}.module.ts`;
-    
-    fs.writeFile(controllerFile,controllerString,(err)=>{
-        if(err) throw err;
-        console.log('...generated controller...')
-    })
-    fs.writeFile(entityFile,entityString,(err)=>{
-        if(err) throw err;
-        console.log('...generated entity...')
-    })
-    fs.writeFile(serviceFile,serviceString,(err)=>{
-        if(err) throw err;
-        console.log('...generated service...')
-    })
-    fs.writeFile(moduleFile,moduleString,(err)=>{
-        if(err) throw err;
-        console.log('...generated module...')
-    })
-    
-    break;
-    default:
-    console.log(options)
-    process.exit(1);    
-    break;
-}
+            let controllerFile = `${moduleNameFiles}/${moduleNameFiles}.controller.ts`;
+            let entityFile = `${moduleNameFiles}/${moduleNameFiles}.entity.ts`;
+            let serviceFile = `${moduleNameFiles}/${moduleNameFiles}.service.ts`;
+            let moduleFile = `${moduleNameFiles}/${moduleNameFiles}.module.ts`;
+
+            fs.writeFile(controllerFile, controllerString, (err) => {
+                if (err) throw err;
+                console.log('...generated controller...')
+            })
+            fs.writeFile(entityFile, entityString, (err) => {
+                if (err) throw err;
+                console.log('...generated entity...')
+            })
+            fs.writeFile(serviceFile, serviceString, (err) => {
+                if (err) throw err;
+                console.log('...generated service...')
+            })
+            fs.writeFile(moduleFile, moduleString, (err) => {
+                if (err) throw err;
+                console.log('...generated module...')
+            })
+
+            break;
+        default:
+            console.log(options)
+            process.exit(1);
+            break;
+    }
